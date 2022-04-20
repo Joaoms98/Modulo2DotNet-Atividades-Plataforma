92707b45bf6539077a1c9bb32fa63633dbf506f6
Joaoms98 <jvm1800@gmail.com>
2022-04-20 13:20:06 -0300

    Uploado Exercícios lista3

---
+++ SQL/Exercícios/Exercícios lista 3 SQL SERVER/Classificado SQL/SQL_Classificado_DML.sql
@@ -0,0 +1,16 @@
+INSERT INTO Usuario
+VALUES
+(1, 'Vitor','joano@gmail.com','5454666','11996896545'),
+(2, 'joao victor','jvm1800@gmail.com','123456','11996986548454'),
+(3, 'Joaozinho piriri','jailson@gmail.com','321654','119968877894')
+
+SELECT*FROM Usuario
+
+INSERT INTO Postagem
+VALUES
+(1,'Vaga de emprego para motoboy','Levar meu carro para o conserto UNO2015','lhfjlahfdjkjdgfg5456s1d584fd56s4f64.png',1),
+(2,'Buscando emprego na área de herdeiro','','SDSDSDSD46545646548DSD46S8D4.png',2)
+
+
+SELECT * FROM Postagem
+where Fk_Usuario LIKE 1
